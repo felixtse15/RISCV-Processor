658301ebba2b658963669f0d90ea525469a568ce
felixtse15 <146990753+felixtse15@users.noreply.github.com>
2025-07-30 10:36:56 -0700

    Update run_test.tcl

--- src/run_test.tcl
+++ src/run_test.tcl
@@ -1,5 +1,6 @@
-foreach i {1 2 3 4 5 6 7 8 9 10} {
-    set memfile "D:/projects/RISCV/tools/input/input_$i.txt"
-    vlog -work work ../src/*.sv
-    vsim -c work.rvsimtb +memfile=$memfile -do "run -all; quit"
-}
+#
+# foreach i {1 2 3 4 5 6 7 8 9 10} {
+#    set memfile "D:/projects/RISCV/tools/input/input_$i.txt"
+#    vlog -work work ../src/*.sv
+#    vsim -c work.rvsimtb +memfile=$memfile -do "run -all; quit"
+# }
